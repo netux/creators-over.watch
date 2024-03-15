aef4edd4ac018c2c8d643b26d17347cc7cdb51a5
Martín (Netux) Rodríguez <send.email@netux.site>
2024-03-15 12:51:16 -0300

    refactor(env): change Vite exposed env var prefix "VITE_" → "PUBLIC_"

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,2 +1,2 @@
-export const LAST_BIG_UPDATE_TIMESTAMP = new Date(import.meta.env.VITE_LAST_BIG_UPDATE_TIMESTAMP);
-export const LAST_OKAY_UPDATE_TIMESTAMP = new Date(import.meta.env.VITE_LAST_OKAY_UPDATE_TIMESTAMP);
+export const LAST_BIG_UPDATE_TIMESTAMP = new Date(import.meta.env.PUBLIC_LAST_BIG_UPDATE_TIMESTAMP);
+export const LAST_OKAY_UPDATE_TIMESTAMP = new Date(import.meta.env.PUBLIC_LAST_OKAY_UPDATE_TIMESTAMP);
