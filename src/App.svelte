43723185d9d6bdb2cae24c5e1ff1dcb097b1abc9
Martín (Netux) Rodríguez <send.email@netux.site>
2024-03-15 12:42:40 -0300

    refactor(app): .header → .before, .footer → .after

--- src/App.svelte
+++ src/App.svelte
@@ -5,28 +5,28 @@
 </script>
 
 <style lang="scss">
-  .header, .counter, .footer {
+  .before, .counter, .after {
     margin: 1rem 0;
   }
 
-  .header {
+  .before {
     font-size: 1.5rem;
   }
 </style>
 
-<div class="header">It's been</div>
+<div class="before">It's been</div>
 <div class="counter">
   <FancyCounter since={env.LAST_BIG_UPDATE_TIMESTAMP} />
 </div>
-<div class="footer">
-  since the last big
+<div class="after">
+  since the last important
   <a href={env.LAST_BIG_UPDATE_PATCH_NOTES_LINK} target="_blank">Overwatch Workshop update</a>
 </div>
 
 <hr />
 
 <small>
-  And
+  and
   <SimpleCounter since={env.LAST_OKAY_UPDATE_TIMESTAMP} />
   since the last moderately okay
   <a href={env.LAST_OKAY_UPDATE_PATCH_NOTES_LINK} target="_blank">Overwatch Workshop update</a>
