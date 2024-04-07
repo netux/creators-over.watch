893d9dacaee7c88809ee2071eb2f7e01e9a9ef2a
netux <martin.rodriguez+github-com@netux.site>
2024-04-06 23:15:47 -0300

    feat(app): add icon to external links

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,7 @@
   import { LAST_BIG_UPDATE_TIMESTAMP, LAST_OKAY_UPDATE_TIMESTAMP } from './lib/env';
   import FancyCounter from './lib/FancyCounter.svelte';
   import SimpleCounter from './lib/SimpleCounter.svelte';
+  import ExternalAnchor from './lib/ExternalAnchor.svelte';
   import { padLeftWithZeroes } from './lib/util';
 
   function getPatchNoteLink(update: Date) {
@@ -35,7 +36,7 @@
 </div>
 <div class="after">
   since the last important
-  <a href={getPatchNoteLink(LAST_BIG_UPDATE_TIMESTAMP)} target="_blank">Overwatch Workshop update</a>
+  <ExternalAnchor href={getPatchNoteLink(LAST_BIG_UPDATE_TIMESTAMP)}>Overwatch Workshop update</ExternalAnchor>
 </div>
 
 {#if LAST_OKAY_UPDATE_TIMESTAMP}
@@ -45,6 +46,6 @@
     and
     <SimpleCounter since={LAST_OKAY_UPDATE_TIMESTAMP} />
     since the last moderately okay
-    <a href={getPatchNoteLink(LAST_OKAY_UPDATE_TIMESTAMP)} target="_blank">Overwatch Workshop update</a>
+    <ExternalAnchor href={getPatchNoteLink(LAST_OKAY_UPDATE_TIMESTAMP)}>Overwatch Workshop update</ExternalAnchor>
   </small>
 {/if}
---
+++ src/lib/ExternalAnchor.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import Exit from 'svelte-ionicons/ExitOutline.svelte';
+	import type { HTMLAnchorAttributes, HTMLAttributeAnchorTarget } from 'svelte/elements';
+
+	interface $$Props extends HTMLAnchorAttributes {
+		target?: HTMLAttributeAnchorTarget | undefined;
+	}
+
+	export let target = "_blank";
+</script>
+
+<style lang="scss">
+	.icon {
+		margin-left: 0.5ch;
+
+		:global(> svg) {
+			width: 1em;
+			height: 1em;
+			vertical-align: text-bottom;
+		}
+	}
+</style>
+
+<!-- svelte-ignore a11y-missing-attribute -->
+<a {target} { ... $$restProps}>
+	<slot /><span class="icon"><Exit /></span>
+</a>
