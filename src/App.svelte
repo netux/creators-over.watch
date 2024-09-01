aed378063feaede73af57dae617a5e6fa6e3ddcf
Martín (Netux) Rodríguez <send.email@netux.site>
2024-09-01 00:54:43 -0300

    refactor(app): move counters to their own HomePage component
    Only leave the footer banner in <App>

--- src/App.svelte
+++ src/App.svelte
@@ -1,22 +1,6 @@
 <script lang="ts">
-  import {
-    LAST_BIG_UPDATE_TIMESTAMP,
-    LAST_OKAY_UPDATE_TIMESTAMP,
-    FOOTER_BANNER_TEXT
-  } from './lib/env';
-  import FancyCounter from './lib/FancyCounter.svelte';
-  import SimpleCounter from './lib/SimpleCounter.svelte';
-  import ExternalAnchor from './lib/ExternalAnchor.svelte';
-  import { padLeftWithZeroes } from './lib/util';
-
-  function getPatchNoteLink(update: Date) {
-    const year = update.getFullYear(),
-          month = update.getMonth() + 1,
-          day = update.getDate();
-    return "https://overwatch.blizzard.com/en-us/news/patch-notes/live/" +
-      `${year}/${padLeftWithZeroes(month)}` +
-      `#patch-${padLeftWithZeroes(year, 4)}-${padLeftWithZeroes(month)}-${padLeftWithZeroes(day)}`
-  }
+	import HomePage from './HomePage.svelte';
+  import { FOOTER_BANNER_TEXT } from './lib/env';
 </script>
 
 <style lang="scss">
@@ -25,29 +9,12 @@
     --App__footer-outline-color: #5d5d5d;
 
     @media (prefers-color-scheme: light) {
-      --App__footer-text-color: var(--text-color);
+      --App__footer-text-color: var(--primary-text-color);
       // --App__footer-outline-color: /* same */;
     }
   }
 
-  .before, .big-counter, .after {
-    margin: 1rem 0;
-  }
-
-  .before {
-    font-size: 1.5rem;
-  }
-
-  .big-counter {
-    width: fit-content;
-    margin: 0 auto;
-  }
-
-  footer {
-    position: fixed;
-    bottom: 0;
-    left: 0;
-    width: 100%;
+  .footer-banner {
     padding: 0.5rem 0.25rem;
     text-align: left;
     font-size: 0.8rem;
@@ -56,28 +23,10 @@
   }
 </style>
 
-<div class="before">It's been</div>
-<div class="big-counter">
-  <FancyCounter since={LAST_BIG_UPDATE_TIMESTAMP} />
-</div>
-<div class="after">
-  since the last important
-  <ExternalAnchor href={getPatchNoteLink(LAST_BIG_UPDATE_TIMESTAMP)}>Overwatch Workshop update</ExternalAnchor>
-</div>
-
-{#if LAST_OKAY_UPDATE_TIMESTAMP}
-  <hr />
-
-  <small>
-    and
-    <SimpleCounter since={LAST_OKAY_UPDATE_TIMESTAMP} />
-    since the last moderately okay
-    <ExternalAnchor href={getPatchNoteLink(LAST_OKAY_UPDATE_TIMESTAMP)}>Overwatch Workshop update</ExternalAnchor>
-  </small>
-{/if}
+<HomePage />
 
 {#if FOOTER_BANNER_TEXT}
-  <footer>
+  <footer class="footer-banner">
     { FOOTER_BANNER_TEXT }
   </footer>
 {/if}
---
+++ src/HomePage.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+  import {
+    LAST_BIG_UPDATE_TIMESTAMP,
+    LAST_OKAY_UPDATE_TIMESTAMP
+  } from './lib/env';
+  import FancyCounter from './lib/FancyCounter.svelte';
+  import SimpleCounter from './lib/SimpleCounter.svelte';
+  import ExternalAnchor from './lib/ExternalAnchor.svelte';
+  import { getPatchNoteLink } from './lib/util';
+</script>
+
+<style lang="scss">
+  .container {
+    margin: 0 auto;
+    max-width: 1280px;
+    margin: auto;
+    padding: 2rem;
+    text-align: center;
+    place-self: center;
+  }
+
+  .before, .big-counter, .after {
+    margin: 1rem 0;
+  }
+
+  .before {
+    font-size: 1.5rem;
+  }
+
+  .big-counter {
+    width: fit-content;
+    margin: 0 auto;
+  }
+</style>
+
+<div class="container">
+  <div class="before">It's been</div>
+  <div class="big-counter">
+    <FancyCounter since={LAST_BIG_UPDATE_TIMESTAMP} />
+  </div>
+  <div class="after">
+    since the last important
+    <ExternalAnchor href={getPatchNoteLink(LAST_BIG_UPDATE_TIMESTAMP)}>Overwatch Workshop update</ExternalAnchor>
+  </div>
+
+  {#if LAST_OKAY_UPDATE_TIMESTAMP}
+    <hr />
+
+    <small>
+      and
+      <SimpleCounter since={LAST_OKAY_UPDATE_TIMESTAMP} />
+      since the last moderately okay
+      <ExternalAnchor href={getPatchNoteLink(LAST_OKAY_UPDATE_TIMESTAMP)}>Overwatch Workshop update</ExternalAnchor>
+    </small>
+  {/if}
+</div>
