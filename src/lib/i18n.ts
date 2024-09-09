764847404a54fa39e92de3ed1eb969641a3bdc0b
Martín (Netux) Rodríguez <send.email@netux.site>
2024-09-08 23:22:13 -0300

    feat(i18n): add Spanish locale
    Translated by yours truly.

--- src/lib/i18n.ts
+++ src/lib/i18n.ts
@@ -1,6 +1,7 @@
 import { init, register, getLocaleFromNavigator } from 'svelte-i18n';
 
 register('en', () => import('$assets/i18n/en.json'));
+register('es', () => import('$assets/i18n/es.json'));
 
 init({
   fallbackLocale: 'en',
